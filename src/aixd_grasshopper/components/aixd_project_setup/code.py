8c1d47f7c25acbeaab9c5fbcaf062fc0916bce90
Aleksandra Apolinarska <apolinarska@arch.ethz.ch>
2024-02-28 09:37:00 +0100

    #87 setting up components

---
+++ src/aixd_grasshopper/components/aixd_project_setup/code.py
@@ -0,0 +1,19 @@
+import os
+from scriptcontext import sticky as st
+from aixd_grasshopper.gh_ui import project_setup
+from aixd_grasshopper.gh_ui_helper import session_id, component_id
+
+
+complete_path = os.path.join(project_folder, dataset_name)
+if not os.path.exists(complete_path):
+    print("The path {} does not exist and will be now created.".format(complete_path))
+    os.mkdir(complete_path)
+
+cid = component_id(ghenv.Component, "project_setup")
+
+if set: 
+    st[cid] = project_setup(session_id(), project_folder, dataset_name)
+
+if cid in st.keys():
+    msg = st[cid]['msg']
+    path = st[cid]['path']
